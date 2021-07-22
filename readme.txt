8499c619227de9bee00eb6e2007f4538464fdf7c
Luis Fernando <fernandocf_j@outlook.com>
2021-07-22 00:29:04 -0500

    correcion config.js

--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 las peticiones llegan a network luego al controlador y estas hacen el llamado a el store
 
 peticion -> network -> controller -> store(db)
+
+
+los micro servicios son llamados en el archivo -->[user/ index.js] como  [store] que requiere -> remote-mysql
--- store/remote.js
+++ store/remote.js
@@ -2,7 +2,7 @@
 const request = require('request')
 
 
-function createRemoteDB(host, port){
+function createRemoteDB(host, port) {
     const URL = 'http://'+ host + ':' + port;
 
     function list (table){
@@ -13,23 +13,24 @@ function createRemoteDB(host, port){
     // function insert(table, data)
     // function query(table, query, join)
 
-    function req(method, table, data){
+    function req(method, table, data) {
         let url = URL + '/' + table;
         body = '';
 
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
             request({
                 method,
-                headers:{
+                headers: {
                     'content-type': 'application/json'
                 },
                 url,
                 body,
-            }, (err, req, body)=>{
-                if(err){
-                    console.error('Error de la base de datos remota', err);
+            }, (err, req, body) => {
+                if (err) {
+                    console.error('Error con la base de datos remota', err);
                     return reject(err.message);
                 }
+
                 const resp = JSON.parse(body);
                 return resolve(resp.body);
             })
